3e1f81b4c59990638b2532503a7b263b7b707220
Brad <bradleyevans.x@gmail.com>
2023-07-21 17:30:27 -0400

    adding emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Game } from "../hooks/useGames";
 import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
+import Emoji from "./Emoji";
 import PlatformIconList from "./PlatformIconList";
 
 interface Props {
@@ -19,7 +20,9 @@ const GameCard = ({ game }: Props) => (
         ></PlatformIconList>
         <CriticScore score={game.metacritic}></CriticScore>
       </HStack>
-      <Heading fontSize={"2xl"}>{game.name}</Heading>
+      <Heading justifyContent={"center"} textAlign={"center"} fontSize={"2xl"}>
+        {game.name} <Emoji rating={game.rating_top}></Emoji>
+      </Heading>
     </CardBody>
   </Card>
 );
